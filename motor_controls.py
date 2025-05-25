63deadbadda46ec1c09b4e66c7afc544b60202e2
rojasj84 <rojasj84@gmail.com>
2025-05-25 13:54:31 -0400

    adding command format for aerotech axis commands

--- motor_controls.py
+++ motor_controls.py
@@ -51,50 +51,75 @@ class ObjectivePositionPanel(tk.Frame):
         travel_label = tk.Label(self, text="XY TRAVEL")
         travel_label.place(x = 5, y = 5, width=frame_width-10, height=25)
 
-        self.jog_up_button_image = ImageTk.PhotoImage(scale_images(Path(r"images\up.png"),4))
+        self.jog_up_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/up.png"),4))
         jog_up = tk.Button(self, text="UP", image=self.jog_up_button_image, relief=tk.FLAT, background=win_color)
         jog_up.place(x = cross_center_x_position - 1*(button_short_dimension)/2, y = cross_center_y_position - 1*button_long_dimension - button_short_dimension, width = button_short_dimension-button_spacing, height = button_long_dimension-button_spacing)
-        jog_up.bind("<ButtonPress>", lambda event:  self.motor_motion(1))
+        jog_up.bind("<ButtonPress>", lambda event:  self.motor_motion("Y","+"))
         #jog_up.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
-        self.jog_down_button_image = ImageTk.PhotoImage(scale_images(Path(r"images\down.png"),4))
+        self.jog_down_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/down.png"),4))
         jog_down = tk.Button(self, text="DOWN", image=self.jog_down_button_image, relief=tk.FLAT, background=win_color)
         jog_down.place(x = cross_center_x_position - 1*(button_short_dimension)/2, y = cross_center_y_position, width = button_short_dimension-button_spacing, height = button_long_dimension-button_spacing)
-        jog_down.bind("<ButtonPress>", lambda event:  self.motor_motion(1))
+        jog_down.bind("<ButtonPress>", lambda event:  self.motor_motion("Y","-"))
         #jog_down.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
-        self.jog_right_button_image = ImageTk.PhotoImage(scale_images(Path(r"images\right.png"),4))
+        self.jog_right_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/right.png"),4))
         jog_right = tk.Button(self, text="RIGHT", image=self.jog_right_button_image, relief=tk.FLAT, background=win_color)
         jog_right.place(x = cross_center_x_position - 0*button_long_dimension + 1*(button_short_dimension)/2, y = cross_center_y_position - button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
-        jog_right.bind("<ButtonPress>", lambda event:  self.motor_motion(1))
+        jog_right.bind("<ButtonPress>", lambda event:  self.motor_motion("X","+"))
         #jog_right.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
-        self.jog_left_button_image = ImageTk.PhotoImage(scale_images(Path(r"images\left.png"),4))
+        self.jog_left_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/left.png"),4))
         jog_left = tk.Button(self, text="LEFT", image=self.jog_left_button_image, relief=tk.FLAT, background=win_color)
         jog_left.place(x = cross_center_x_position - 1*button_long_dimension - 1*(button_short_dimension)/2, y = cross_center_y_position - button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
-        jog_left.bind("<ButtonPress>", lambda event:  self.motor_motion(1))
+        jog_left.bind("<ButtonPress>", lambda event:  self.motor_motion("Y","-"))
         #jog_left.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         focus_label = tk.Label(self, text="FOCUS")
         focus_label.place(x = 5, y = 200, width=frame_width-10, height=25)
 
-        self.jog_focusin_button_image = ImageTk.PhotoImage(scale_images(Path(r"images\focus-in.png"),4))
+        self.jog_focusin_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/focus-in.png"),4))
         focus_in = tk.Button(self, text="FOCUS IN", image=self.jog_focusin_button_image, relief=tk.FLAT, background=win_color)
         focus_in.place(x = cross_center_x_position - 1*button_long_dimension - 1*(button_short_dimension)/2, y = 200 + button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
-        focus_in.bind("<ButtonPress>", lambda event:  self.motor_motion(1))
+        focus_in.bind("<ButtonPress>", lambda event:  self.motor_motion("Z","+"))
         #focus_in.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
-        self.jog_focusout_button_image = ImageTk.PhotoImage(scale_images(Path(r"images\focus-out.png"),4))
+        self.jog_focusout_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/focus-out.png"),4))
         focus_out = tk.Button(self, text="FOCUS OUT", image=self.jog_focusout_button_image, relief=tk.FLAT, background=win_color)
         focus_out.place(x = cross_center_x_position - 0*button_long_dimension + 1*(button_short_dimension)/2, y = 200 + button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
-        focus_out.bind("<ButtonPress>", lambda event:  self.motor_motion(1))
+        focus_out.bind("<ButtonPress>", lambda event:  self.motor_motion("Z","-"))
         #focus_out.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
-    def motor_motion(self, motor_state):
-        if motor_state == 1:
+        step_size_label = tk.Label(self, text="Step Size (μm)")
+        step_size_label.place(x = 0, y = 285, width = 100, height=25)
+
+        self.step_size_text = tk.Text(self)
+        self.step_size_text.place(x = 100, y = 285, width = 50, height=25)
+        self.step_size_text.insert(tk.END, "5")
+
+        step_velocity_label = tk.Label(self, text="Velocity (μm/s)")
+        step_velocity_label.place(x = 0, y = 315, width = 100, height=25)
+
+        self.step_velocity_text = tk.Text(self)
+        self.step_velocity_text.place(x = 100, y = 315, width = 50, height=25)
+        self.step_velocity_text.insert(tk.END, "2")
+
+
+    def motor_motion(self, axis, direction):
+        '''if motor_state == 1:
             print("1")
         elif motor_state == 0:
-            print("0")
+            print("0")'''
+        
+        step_size = self.step_size_text.get("1.0", "end-1c") 
+        velocity = self.step_velocity_text.get("1.0", "end-1c")
+
+        if direction == "+":
+             command_string = "MOVEINC" + axis + step_size + "F" + axis + velocity
+        elif direction == "-":
+            command_string = "MOVEINC" + axis + "-" + step_size + "F" + axis + velocity
+                
+        print(command_string)
 
 if __name__ == "__main__":
 
