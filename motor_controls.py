ba7b8dbf3942032d55a09bd2bdd0e367621ea787
rojasj84 <rojasj84@gmail.com>
2025-05-26 13:02:28 -0400

    changed the placing to .grid

--- motor_controls.py
+++ motor_controls.py
@@ -34,7 +34,7 @@ class ObjectivePositionPanel(tk.Frame):
         super().__init__(container)
         
         frame_width = 200
-        frame_height = 400
+        frame_height = 360
 
         button_spacing = 5
         button_long_dimension = 223/4 + button_spacing
@@ -47,61 +47,75 @@ class ObjectivePositionPanel(tk.Frame):
         # build labels and buttons
         self.config(background=win_color, highlightbackground="black", highlightthickness=1, relief="raised")
         self.place(x=x_location,y=y_location,width = frame_width,height = frame_height)
+        #self.pack(pady=10)
+        
 
         travel_label = tk.Label(self, text="XY TRAVEL")
-        travel_label.place(x = 5, y = 5, width=frame_width-10, height=25)
+        #travel_label.place(x = 5, y = 5, width=frame_width-10, height=25)
+        travel_label.grid(row = 0, column = 1, columnspan=3)
 
         self.jog_up_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/up.png"),4))
         jog_up = tk.Button(self, text="UP", image=self.jog_up_button_image, relief=tk.FLAT, background=win_color)
-        jog_up.place(x = cross_center_x_position - 1*(button_short_dimension)/2, y = cross_center_y_position - 1*button_long_dimension - button_short_dimension, width = button_short_dimension-button_spacing, height = button_long_dimension-button_spacing)
+        #jog_up.place(x = cross_center_x_position - 1*(button_short_dimension)/2, y = cross_center_y_position - 1*button_long_dimension - button_short_dimension, width = button_short_dimension-button_spacing, height = button_long_dimension-button_spacing)
+        jog_up.grid(row = 1, column = 2)
         jog_up.bind("<ButtonPress>", lambda event:  self.motor_motion("Y","+"))
         #jog_up.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         self.jog_down_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/down.png"),4))
         jog_down = tk.Button(self, text="DOWN", image=self.jog_down_button_image, relief=tk.FLAT, background=win_color)
-        jog_down.place(x = cross_center_x_position - 1*(button_short_dimension)/2, y = cross_center_y_position, width = button_short_dimension-button_spacing, height = button_long_dimension-button_spacing)
+        jog_down.grid(row = 3, column = 2)
+        #jog_down.place(x = cross_center_x_position - 1*(button_short_dimension)/2, y = cross_center_y_position, width = button_short_dimension-button_spacing, height = button_long_dimension-button_spacing)
         jog_down.bind("<ButtonPress>", lambda event:  self.motor_motion("Y","-"))
         #jog_down.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         self.jog_right_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/right.png"),4))
         jog_right = tk.Button(self, text="RIGHT", image=self.jog_right_button_image, relief=tk.FLAT, background=win_color)
-        jog_right.place(x = cross_center_x_position - 0*button_long_dimension + 1*(button_short_dimension)/2, y = cross_center_y_position - button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        #jog_right.place(x = cross_center_x_position - 0*button_long_dimension + 1*(button_short_dimension)/2, y = cross_center_y_position - button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        jog_right.grid(row = 2, column = 3)
         jog_right.bind("<ButtonPress>", lambda event:  self.motor_motion("X","+"))
         #jog_right.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         self.jog_left_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/left.png"),4))
         jog_left = tk.Button(self, text="LEFT", image=self.jog_left_button_image, relief=tk.FLAT, background=win_color)
-        jog_left.place(x = cross_center_x_position - 1*button_long_dimension - 1*(button_short_dimension)/2, y = cross_center_y_position - button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
-        jog_left.bind("<ButtonPress>", lambda event:  self.motor_motion("Y","-"))
+        #jog_left.place(x = cross_center_x_position - 1*button_long_dimension - 1*(button_short_dimension)/2, y = cross_center_y_position - button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        jog_left.grid(row = 2, column = 1)
+        jog_left.bind("<ButtonPress>", lambda event:  self.motor_motion("X","-"))
         #jog_left.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         focus_label = tk.Label(self, text="FOCUS")
-        focus_label.place(x = 5, y = 200, width=frame_width-10, height=25)
+        #focus_label.place(x = 5, y = 200, width=frame_width-10, height=25)
+        focus_label.grid(row = 4, column = 1, columnspan=3)
 
         self.jog_focusin_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/focus-in.png"),4))
         focus_in = tk.Button(self, text="FOCUS IN", image=self.jog_focusin_button_image, relief=tk.FLAT, background=win_color)
-        focus_in.place(x = cross_center_x_position - 1*button_long_dimension - 1*(button_short_dimension)/2, y = 200 + button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        #focus_in.place(x = cross_center_x_position - 1*button_long_dimension - 1*(button_short_dimension)/2, y = 200 + button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        focus_in.grid(row = 5, column = 0, columnspan=2, sticky='e', padx=10)
         focus_in.bind("<ButtonPress>", lambda event:  self.motor_motion("Z","+"))
         #focus_in.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         self.jog_focusout_button_image = ImageTk.PhotoImage(scale_images(Path(r"images/focus-out.png"),4))
         focus_out = tk.Button(self, text="FOCUS OUT", image=self.jog_focusout_button_image, relief=tk.FLAT, background=win_color)
-        focus_out.place(x = cross_center_x_position - 0*button_long_dimension + 1*(button_short_dimension)/2, y = 200 + button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        #focus_out.place(x = cross_center_x_position - 0*button_long_dimension + 1*(button_short_dimension)/2, y = 200 + button_short_dimension , width = button_long_dimension-button_spacing, height = button_short_dimension-button_spacing)
+        focus_out.grid(row = 5, column = 3, columnspan=2, sticky='w', padx=10)
         focus_out.bind("<ButtonPress>", lambda event:  self.motor_motion("Z","-"))
         #focus_out.bind("<ButtonRelease>", lambda event:   self.motor_motion(0))
 
         step_size_label = tk.Label(self, text="Step Size (μm)")
-        step_size_label.place(x = 0, y = 285, width = 100, height=25)
+        #step_size_label.place(x = 0, y = 285, width = 100, height=25)
+        step_size_label.grid(row = 6, column = 0, columnspan=5)
 
-        self.step_size_text = tk.Text(self)
-        self.step_size_text.place(x = 100, y = 285, width = 50, height=25)
+        self.step_size_text = tk.Entry(self)
+        #self.step_size_text.place(x = 100, y = 285, width = 50, height=25)
+        self.step_size_text.grid(row = 7, column = 0, columnspan=5)
         self.step_size_text.insert(tk.END, "5")
 
         step_velocity_label = tk.Label(self, text="Velocity (μm/s)")
-        step_velocity_label.place(x = 0, y = 315, width = 100, height=25)
+        #step_velocity_label.place(x = 0, y = 315, width = 100, height=25)
+        step_velocity_label.grid(row = 8, column = 0, columnspan=5)
 
-        self.step_velocity_text = tk.Text(self)
-        self.step_velocity_text.place(x = 100, y = 315, width = 50, height=25)
+        self.step_velocity_text = tk.Entry(self)
+        #self.step_velocity_text.place(x = 100, y = 315, width = 50, height=25)
+        self.step_velocity_text.grid(row = 9, column = 0, columnspan=5)
         self.step_velocity_text.insert(tk.END, "2")
 
 
@@ -111,8 +125,8 @@ class ObjectivePositionPanel(tk.Frame):
         elif motor_state == 0:
             print("0")'''
         
-        step_size = self.step_size_text.get("1.0", "end-1c") 
-        velocity = self.step_velocity_text.get("1.0", "end-1c")
+        step_size = self.step_size_text.get() 
+        velocity = self.step_velocity_text.get()
 
         if direction == "+":
              command_string = "MOVEINC" + axis + step_size + "F" + axis + velocity
@@ -128,7 +142,7 @@ if __name__ == "__main__":
     # Begin code with window code
     window = tk.Tk()
     window.title("Motor Control Gui")
-    window.geometry("210x410")
+    window.geometry("210x390")
 
     A = ObjectivePositionPanel(window, 5, 5)
 
