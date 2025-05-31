03abca836bf82b1cb36ba7d67f9f706716df3896
rojasj84 <rojasj84@gmail.com>
2025-05-31 09:04:40 -0400

    more dll experiments

--- aerotec_comm_dll.py
+++ aerotec_comm_dll.py
@@ -12,6 +12,7 @@ import clr
 assemblydir = "aerotech_dotnet_dlls"
 assembly_1 = "Aerotech.Ensemble"
 assembly_2= "Aerotech.Common"
+assembly_3= "EnsembleCore64"
 
 
 #sys.path.insert(0, "C:/Windows/System32")
@@ -21,6 +22,7 @@ sys.path.insert(0, assemblydir)
 
 clr.AddReference(assembly_1)
 clr.AddReference(assembly_2)
+#clr.AddReference(assembly_3)
 
 import Aerotech.Ensemble # type: ignore
 import Aerotech.Common # type: ignore
